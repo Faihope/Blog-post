3e5545390c9c1d63a47281d68cce9143c0427d16
Fai <chepngetichfaith46@gmail.com>
2021-06-21 13:32:27 +0300

    Add update and delete functions and routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from ..request import get_quotes
 from app.main.forms import RegistrationForm
-from flask import render_template,redirect,flash,url_for,Blueprint,request
+from flask import render_template,abort,redirect,flash,url_for,Blueprint,request
 from . import main
 from .. import app,db
 import secrets
@@ -109,18 +109,44 @@ def new_post():
         db.session.commit()
         flash("Your post has been created",'success')
         return redirect(url_for('.home'))
-    return render_template('create_post.html',form=form)
+    return render_template('create_post.html',form=form,legend='New Post')
 
-# @main.route('/post/<int:post_id>')
-# def post(post_id):
-#     post = Post.query.get_or_404(post_id)
-#     return render_template('post.html', title=post.title, post=post)
+@main.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
 
-# @main.route('/quotes')
-# def quotes():
-#     quote=get_quotes()
+   
+@main.route('/post/<int:post_id>/update',methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author !=current_user:
+        os.abort(403)
+    form=PostForm()
+    if form.validate_on_submit():
+        post.title=form.title.data
+        post.content=form.content.data
+        db.session.commit()
+        flash("Your post has been updated",'success')
+        return redirect(url_for('.post',post_id=post_id))
+    elif request.method=='GET':
+        form.title.data=post.title
+        form.content.data=post.content
     
-#     return render_template('quotes.html',quote=quote)
+
     
-    
+    return render_template('create_post.html', title='Update Post',legend='Update Post', post=post,form=form)
+
+@main.route('/post/<int:post_id>/delete',methods=['POST'])
+@login_required
+def delete_post(post_id): 
+    post = Post.query.get_or_404(post_id)
+    if post.author !=current_user:
+        os.abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted",'success')
+    return redirect(url_for('.home'))
+ 
 
